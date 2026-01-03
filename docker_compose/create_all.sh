9098f04ee4c1ee1d370b69727e98092e29558a15
Pete02 <petrik.rasanen@gmail.com>
2026-01-03 12:10:37 +0200

    moved booklegion v 0.2 to production

--- docker_compose/create_all.sh
+++ docker_compose/create_all.sh
@@ -2,4 +2,4 @@ docker network create --driver bridge web
 docker compose -f portainer.yaml up -d
 docker compose -f traefik.yaml up -d
 docker compose -f vaultwarden.yaml up -d
-docker compose -f booklegion.yaml up -d
+docker-compose up -f booklegion.yaml -d --pull always
