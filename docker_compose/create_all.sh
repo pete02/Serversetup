b90e9401992641c8c51671b837a4f52a2b15c561
Pete02 <petrik.rasanen@gmail.com>
2026-01-19 17:56:27 +0200

    unifi is working etc.

--- docker_compose/create_all.sh
+++ docker_compose/create_all.sh
@@ -1,5 +1,7 @@
 docker network create --driver bridge web
 docker compose -f portainer.yaml up -d
+docker compose -f mongo.yaml up -d
 docker compose -f traefik.yaml up -d
 docker compose -f vaultwarden.yaml up -d
-docker-compose up -f booklegion.yaml -d --pull always
+docker compose -f ubiqiti.yaml up -d
+docker compose -f booklegion.yaml up -d --pull always
