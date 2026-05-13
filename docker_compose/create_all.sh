00d217aff37a7b4c4d42e977592e06979b8687b2
pete02 <petrik.rasanen@gmail.com>
2026-05-13 15:54:44 +0300

    added porkbun ddns

--- docker_compose/create_all.sh
+++ docker_compose/create_all.sh
@@ -4,4 +4,5 @@ docker compose -f mongo.yaml up -d
 docker compose -f traefik.yaml up -d
 docker compose -f vaultwarden.yaml up -d
 docker compose -f ubiqiti.yaml up -d
+docker compose -f porkbunddns.yaml up -d
 docker compose -f booklegion.yaml up -d --pull always
--- docker_compose/destroy_all.sh
+++ docker_compose/destroy_all.sh
@@ -2,3 +2,6 @@ docker compose -f portainer.yaml down
 docker compose -f traefik.yaml down
 docker compose -f vaultwarden.yaml down
 docker compose -f booklegion.yaml down
+docker compose -f porkbunddns.yaml down
+docker compose -f mongo.yaml down
+docker compose -f ubiqiti.yaml down
